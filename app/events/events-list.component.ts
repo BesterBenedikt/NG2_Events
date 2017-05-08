b5c022e6776bb9b229094a9001e25923843c6d2c
Benedikt Schleibinger <b.schleibinger@avanade.com>
2017-05-08 20:36:19 +0200

    Added: *ngFor

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -7,5 +7,5 @@ import {Component} from  '@angular/core'
 
 export class EventsListComponent {
 
-    event_export = {name:"Football", location:"Munich"}
+    events_export = [{name:"Football", location:"Munich"}, {name:"Gymnastics",location:"Berlin"},{name:"Baseball",location:"NYC"}]
 }
