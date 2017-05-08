417b3161a42e59828aa99f7d30db966177541be8
Benedikt Schleibinger <b.schleibinger@avanade.com>
2017-05-08 20:47:19 +0200

    Added: *ngIf

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -7,5 +7,5 @@ import {Component} from  '@angular/core'
 
 export class EventsListComponent {
 
-    events_export = [{name:"Football", location:"Munich"}, {name:"Gymnastics",location:"Berlin"},{name:"Baseball",location:"NYC"}]
+    events_export = [{name:"Football", location:"Munich"}, {name:"Gymnastics",location:"Berlin"},{name:"Baseball",location:"NYC"},{location:"nowhere    "}]
 }
