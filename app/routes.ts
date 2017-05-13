03cd276112d72b729db7a9901b33235eb04e3e24
Benedikt Schleibinger <b.schleibinger@avanade.com>
2017-05-13 22:58:43 +0200

    Resolved: New Route

--- app/routes.ts
+++ app/routes.ts
@@ -6,8 +6,9 @@ import {Error404Component} from './Error/404.component'
 import {EventRouteActivator} from './events/events-details/event-route-activator.service'
 
 export const appRoutes:Routes = [
+        {path:'events/new',component:EventCreateComponent},
     {path:'events',component:EventsListComponent},
-    {path:'events/new',component:EventCreateComponent},
+
     {path:'events/:id',component:EventsDetailsComponent, canActivate:[EventRouteActivator]},
     {path:'404',component:Error404Component},   
     {path:'', redirectTo:'events', pathMatch:'full'}
