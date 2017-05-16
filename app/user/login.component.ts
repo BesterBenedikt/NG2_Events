7a1b115445c909f05a899d068bf5203e01d58fc6
Benedikt Schleibinger <b.schleibinger@avanade.com>
2017-05-16 21:39:29 +0200

    Added: Template-Based-Forms-Validation

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -2,7 +2,8 @@ import {Component} from  '@angular/core'
 import {AuthService} from './auth.service'
 import {Router} from '@angular/router'
 @Component({
-    templateUrl:'./app/user/login.component.html'
+    templateUrl:'./app/user/login.component.html',
+    styles : ['em {float:right;color:	rgb(0, 255, 255)}']
 })
 
 export class LoginComponent {
