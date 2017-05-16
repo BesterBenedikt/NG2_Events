666f5761b9e4b58a02d0e7951ecfa494e696b4fc
Benedikt Schleibinger <b.schleibinger@avanade.com>
2017-05-16 22:43:41 +0200

    Added: Validaton Added for Reactive Form

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,18 +1,52 @@
-import { Component } from '@angular/core'
+import { Component,OnInit } from '@angular/core'
+import {FormGroup,FormControl,Validators} from '@angular/forms'
+import {Router} from '@angular/router'
+import {AuthService} from './auth.service'
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl:'./app/user/profile.component.html',
+  styles:[`
+          em  {float:right; color:#E05C65;padding-left:10px}
+          .error input {background-color:#E3C3C5;}
+          .error ::web-kit-input-placeholder {color:#999;}
+          `],
+
 })
-export class ProfileComponent {
-       
+export class ProfileComponent implements OnInit {
+
+  profileForm:FormGroup
+  firstName:FormControl
+  lastName:FormControl
+
+  constructor(private authService:AuthService, private route:Router) {}
+  ngOnInit(): void {
+    this.firstName = new FormControl(this.authService.currentUser.firstName,Validators.required)
+    this.lastName = new FormControl(this.authService.currentUser.lastName,Validators.required)
+    this.profileForm = new FormGroup({
+      firstName:this.firstName,
+      lastName:this.lastName
+    })
+  }
+
+  cancel(){
+    this.route.navigate(['events'])
+  }
+
+  saveProfile() {
+    if (this.profileForm.valid) 
+    {
+      console.log(this.profileForm.value['firstName'])
+      this.authService.updateCurrentUser(this.profileForm.value['firstName'],this.profileForm.value['lastName'])
+      this.route.navigate(['events'])
+    }
+
+  }
+
+    validateFirstName() {
+        return this.firstName.valid || this.firstName.untouched
+    }
+
+    validateLastName() {
+        return this.lastName.valid || this.lastName.untouched
+    }
 }
--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -2,15 +2,17 @@ import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {RouterModule} from '@angular/router'
 
-/*FormsModule has to be imported in Module*/
-import {FormsModule} from '@angular/forms'
+/*For Using FormsModule has to be imported in Module*/
+/*For Using Reactive ReactiveFormsModule has to be imported in Module*/
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+
 
 import {ProfileComponent} from './profile.component'
 import {userRoutes} from './user.routes'
 import {LoginComponent} from './login.component'
 
 @NgModule({
-    imports: [CommonModule,RouterModule.forChild(userRoutes),FormsModule],
+    imports: [CommonModule,FormsModule,ReactiveFormsModule,RouterModule.forChild(userRoutes)],
     declarations:[ProfileComponent,LoginComponent],
     providers:[]   
 })
